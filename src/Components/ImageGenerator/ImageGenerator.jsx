2b0861b6707fb10a49a24ecd060167cbe877884d
ANCY PETER <ancypeter2k@gmail.com>
2025-09-23 01:03:03 +0530

    AI IMAGE GENERATOR

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from "react";
+import "./ImageGenerator.css";
+import default_image from "../Assets/Robot.png";
+
+function ImageGenerator() {
+  const [imageUrl, setImageUrl] = useState("/");
+  const [loading, setLoading] = useState(false);
+  let inputRef = useRef(null);
+
+  const imageGenerator = async () => {
+    if (!inputRef.current.value) return;
+
+    setLoading(true);
+
+    try {
+      const HUGGING_FACE_API_KEY = process.env.REACT_APP_HF_API_TOKEN;
+      const HUGGING_FACE_MODEL = process.env.REACT_APP_HF_MODEL;
+      const response = await fetch(`https://api-inference.huggingface.co/models/${HUGGING_FACE_MODEL}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json",
+          Authorization : `Bearer ${HUGGING_FACE_API_KEY}`,
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify(
+          {
+            inputs: inputRef.current.value,
+          }
+        )
+      }
+    );
+      const blob = await response.blob();
+      const imageUrl = URL.createObjectURL(blob);
+      setImageUrl(imageUrl);
+    } catch (error) {
+      console.error("Error generating image:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="image-generator">
+      <h2>AI Image Generator</h2>
+      <p>Let’s explore the magic of art with the power of AI!</p>
+
+      <img
+        src={imageUrl === "/" ? default_image : imageUrl}
+        alt="Generated"
+        className="default-image"
+      />
+
+      <div className="input-container">
+        <input
+          type="text"
+          ref={inputRef}
+          placeholder="Describe what you want to see..."
+          className="prompt-input"
+        />
+        <button className="generate-button" onClick={imageGenerator}>
+          {loading ? "Generating..." : "Generate Image"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageGenerator;
